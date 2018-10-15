8b61e9be28eac869fc3b49267832455afab91280
Matthew Poling <polingm2@dewv.edu>
2018-10-15 16:01:38 +0000

    Updates via sync script

--- loader.js
+++ loader.js
@@ -1,6 +1,6 @@
 import {game} from "../sgc/sgc.js";
 
-game.preloadImage("water.png");
+game.preloadImage("Crate.png");
 game.preloadImage("slider.png");
 game.preloadImage("start.png");
 game.preloadImage("finish.png");
@@ -9,3 +9,4 @@ game.preloadImage("wall.png");
 game.preloadImage("ann.png", 48, 48);
 game.preloadImage("bat.png", 48, 48);
 game.preloadImage("spider.png", 48, 48);
+game.preloadImage("Cemetery.png");
--- platformPrincess.js
+++ platformPrincess.js
@@ -1,7 +1,7 @@
 import { game } from "./sgc/sgc.js";
 import { Sprite } from "../sgc/sgc.js";
 
-game.setBackground("water.png", 500, 0);
+game.setBackground("Cemetery.png");
 
 class wall extends Sprite {
     constructor() {
@@ -14,7 +14,6 @@ class wall extends Sprite {
     }
 }
 
-new wall();
 
 class Support extends Sprite {
     constructor(x, y, image) {
@@ -40,27 +39,72 @@ let finishPlatform = new Platform(game.displayWidth - 48 * 2, 400, "finish.png")
 finishPlatform.name = "The finish platform";
 
 
-class Slider extends Platform {
+class Slider1 extends Platform {
     constructor(x, y, angle) {
         super(x, y, "slider.png");
         this.name = "A sliding support";
         this.angle = angle;
         this.speed = 48;
-        this.accelerateOnBounce = true;
+        this.accelerateOnBounce = false;
+
+    }
+    handleGameLoop() {
+        if (this.x <= startPlatform.x + 48 * 3) {
+            this.angle = 90;
+        }
+        if (this.y <= 250) {
+            this.angle = 0;
+        }
+        if (this.x >= finishPlatform.x - 48 * 5) {
+            this.angle = 270;
+        }
+        if (this.y >= finishPlatform.y + 48) {
+            this.angle = 180;
+            if (this.x <= startPlatform.x + 48 * 3) {
+                this.angle = 90;
+            }
+        }
+    }
+}
+class Slider2 extends Platform {
+    constructor(x, y, angle) {
+        super(x, y, "slider.png");
+        this.name = "A sliding support";
+        this.angle = angle;
+        this.speed = 48;
+        this.accelerateOnBounce = false;
+
+    }
+    handleGameLoop() {
+        if (this.x == startPlatform.x + 48 * 3) {
+            this.angle = 90;
+        }
+        if (this.y <= 250) {
+            this.angle = 0;
+        }
+        if (this.x >= finishPlatform.x - 48 * 5) {
+            this.angle = 270;
+        }
+        if (this.y >= finishPlatform.y + 48) {
+            this.angle = 180;
+            if (this.x <= startPlatform.x + 48 * 3) {
+                this.angle = 90;
+            }
+        }
 
     }
 }
+new Slider1(startPlatform.x + 48 * 3, startPlatform.y + 48, 0);
 
-new Slider(startPlatform.x + 48 * 3, startPlatform.y + 48, 0);
 
-new Slider(finishPlatform.x - 48 * 5, finishPlatform.y + 48, 180);
+new Slider2(finishPlatform.x - 48 * 5, finishPlatform.y + 48, 180);
 
 class Princess extends Sprite {
     constructor() {
         super();
         this.setImage("ann.png");
         this.x = 48;
-        this.y = 300;
+        this.y = 96;
         this.speed = 0;
         this.speedWhenWalking = 125;
         this.defineAnimation("left", 9, 11);
@@ -68,13 +112,13 @@ class Princess extends Sprite {
         this.isFalling = false;
 
     }
-    handleSpacebar() {
-        if(!this.isFalling){
-            this.y = this.y - 1.25 * this.height;
+    handleUpArrowKey() {
+        if (!this.isFalling) {
+            this.y = this.y - 2 * this.height;
         }
     }
     handleBoundaryContact() {
-        game.end("Princess Ann has drowned!\n\Better luck next time.");
+        game.end("Princess Ann has fallen!\n\Better luck next time.");
     }
     handleRightArrowKey() {
         this.angle = 0;
@@ -96,16 +140,25 @@ class Princess extends Sprite {
             }
         }
         this.x = Math.max(5, this.x);
+        this.speed = 0;
         this.isFalling = false;
-        let supports = game.getSpritesOverlapping(this.x, this.y + this.height, this.width, 1, Support);
+        let supports = game.getSpritesOverlapping(this.x, this.y + this.height, this.width, 10, Support);
         if (supports.length === 0 || supports[0].y < this.y + this.height) {
             this.isFalling = true;
             this.y = this.y + 4;
         }
+        console.log(supports[0]);
+        if (supports.length > 0 && supports[0]instanceof Platform) {
+            this.angle = supports[0].angle;
+            this.speed = supports[0].speed;
+            this.y = supports[0].y - this.height;
+
+
+        }
     }
 }
-    let ann = new Princess();
-    ann.name = "Princess Ann";
+let ann = new Princess();
+ann.name = "Princess Ann";
 
 class Door extends Sprite {
     constructor() {
@@ -116,7 +169,7 @@ class Door extends Sprite {
         this.accelerateOnBounce = false;
     }
     handleCollision(otherSprite) {
-        if(otherSprite === ann) {
+        if (otherSprite === ann) {
             game.end("Congratulations!\n\nPrincess Ann can now pursue the\nStranger deeper into the castle!");
         }
     }
@@ -124,3 +177,38 @@ class Door extends Sprite {
 
 let exit = new Door();
 exit.name = "The exit";
+
+class Box extends Sprite {
+    constructor(x, y) {
+        super();
+        this.x = x;
+        this.y = y;
+        this.speed = 150;
+        this.speedWhenWalking = 200;
+        this.setImage("Crate.png");
+        this.isFalling = false;
+
+    }
+    handleCollision() {
+        this.x = this.x + 1;
+
+
+    }
+
+    handleBoundaryContact() {
+        game.end("You lost your box!");
+    }
+    handleGameLoop() {
+        this.x = Math.max(5, this.x);
+        this.speed = 0;
+        this.isFalling = false;
+        let supports = game.getSpritesOverlapping(this.x, this.y + this.height, this.width, 1, Support);
+        if (supports.length === 0 || supports[0].y < this.y + this.height) {
+            this.isFalling = true;
+            this.y = this.y + 4;
+        }
+    }
+
+}
+
+//new Box(40, 300);
